d6ab6b8ae56b03d9ff9d2427110a4715d4117053
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2017-11-02 15:45:55 +0100

    Removed Ids
    Ids are automaticlly generated and ignored.

--- MusicDataLayer/MusicInitializer.cs
+++ MusicDataLayer/MusicInitializer.cs
@@ -11,34 +11,34 @@ namespace MusicDataLayer
         {
             var genres = new List<Genre>
             {
-                new Genre() {Id = 0, Name = "Unkown"}
+                new Genre() {Name = "Unkown"}
             };
 
             var artists = new List<Artist>
             {
-                new Artist{Id = 0, Name = "Unkown", Gender = Gender.Other}
+                new Artist{Name = "Unkown", Gender = Gender.Other}
             };
 
             var tracks = new List<Track>
             {
-                new Track {Artists = artists, Genres = genres, Id = 0, Name = "Example track", Length = new TimeSpan(0,0,10), Release = DateTime.Today, TrackLocation = "Unkown"}
+                new Track {Artists = artists, Genres = genres, Name = "Example track", Length = new TimeSpan(0,0,10), Release = DateTime.Today, TrackLocation = "Unkown"}
             };
 
             genres[0].Tracks = tracks;
             artists[0].Tracks = tracks;
 
-            var album = new Album {Id = 0, Name = "Example album", Release = DateTime.Today};
+            var album = new Album { Name = "Example album", Release = DateTime.Today };
             var albumTracks = new List<AlbumTrack>
             {
-                new AlbumTrack{Album = album, Track = tracks[0] , AlbumId = 0, TrackId = 0, TrackNumber = 0}
+                new AlbumTrack{Album = album, Track = tracks[0], TrackNumber = 0}
             };
             album.AlbumTracks = albumTracks;
             tracks[0].AlbumTracks = albumTracks;
 
-            var playlist = new Playlist {Id = 0, Name = "Example play list"};
+            var playlist = new Playlist { Name = "Example play list" };
             var playListTracks = new List<PlayListTrack>
             {
-                new PlayListTrack{Playlist = playlist, Track = tracks[0], PlaylistId = 0, TrackNumber = 0, TrackId = 0}
+                new PlayListTrack{Playlist = playlist, Track = tracks[0], TrackNumber = 0}
             };
             playlist.PlayListTracks = playListTracks;
             tracks[0].PlaylistTracks = playListTracks;
