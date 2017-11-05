e2cf6555d46de032dcaa3f1978cec84469316bc4
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2017-11-05 17:12:16 +0100

    Added data repository for tracks (WIP)

---
+++ MusicDataLayer/Repositories/Interfaces/ITrackRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicDataModels;
+
+namespace MusicDataLayer.Repositories.Interfaces
+{
+    public interface ITrackRepository : IDisposable
+    {
+        IEnumerable<Track> GetTracks();
+        Track GetTrackById(int trackId);
+        void InsertStudent(Track track);
+        void DeleteStudent(int studentId);
+        void Save();
+    }
+}
---
+++ MusicDataLayer/Repositories/TrackRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicDataLayer.Repositories.Interfaces;
+using MusicDataModels;
+
+namespace MusicDataLayer.Repositories
+{
+    class TrackRepository : ITrackRepository
+    {
+        private MusicDbContext db = new MusicDbContext();
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public IEnumerable<Track> GetTracks()
+        {
+            return db.Tracks
+                .Include(track => track.Artists)
+                .Include(track => track.Genres)
+                .ToList();
+        }
+
+        public Track GetTrackById(int trackId)
+        {
+            return db.Tracks
+                .Include(track1 => track1.Artists)
+                .Include(track1 => track1.Genres)
+                .FirstOrDefault(track1 => track1.Id == trackId);
+        }
+
+        public void InsertStudent(Track track, IEnumerable<Artist> artists, IEnumerable<Genre> genres)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteStudent(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
