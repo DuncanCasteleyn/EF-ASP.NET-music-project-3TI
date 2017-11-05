95da2442fa367d05166839fb510809a37fe84f25
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2017-11-05 21:58:18 +0100

    Update default pages

--- MusicApplication/Controllers/HomeController.cs
+++ MusicApplication/Controllers/HomeController.cs
@@ -11,14 +11,14 @@ namespace MusicApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About Music manager.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact page.";
 
             return View();
         }
